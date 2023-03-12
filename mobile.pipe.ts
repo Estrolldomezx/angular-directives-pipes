d7bbe06416c8271b9c84241f24ab74ad5035f163
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2023-03-13 05:56:20 +0700

    Create mobile.pipe.ts

---
+++ mobile.pipe.ts
@@ -0,0 +1,32 @@
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({
+  name: 'mobile',
+})
+export class MobilePipe implements PipeTransform {
+  transform(value: string): string {
+    if (typeof value === 'string') {
+      const _text = value.replace(/-/g, '')
+      const pattern = '___-_______'
+      const patternEx = '-'
+      let returnText = ''
+      for (let i = 0, idx = 0; i < pattern.length; i++) {
+        if (_text[idx]) {
+          if (pattern[i] === patternEx && pattern[i]) {
+            returnText += patternEx
+          } else {
+            if (!_text[idx].match(/\d/g)) {
+              return returnText
+            }
+            returnText += _text[idx]
+            idx++
+          }
+        }
+      }
+
+      return returnText
+    }
+
+    return value
+  }
+}
